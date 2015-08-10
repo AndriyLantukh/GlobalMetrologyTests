9ce8bbc2f7a4e04498f1c014037c3fc32505a9e4
Andriy Lantukh <andriy.lantukh@gmail.com>
2015-08-10 15:37:27 +0300

    Few packages added

---
+++ src/com/softserveinc/ita/globalmetrologytests/tests/TestsConstants.java
@@ -0,0 +1,17 @@
+package com.softserveinc.ita.globalmetrologytests.tests;
+
+/**
+ * This class contain utility constants for tests.
+ * @author Dp-076 ATQC
+ *
+ */
+public final class TestsConstants {
+
+    /**
+     * Not called constructor for utility class.
+     */
+    private TestsConstants() {
+    }
+
+   
+}
